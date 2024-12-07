521a5526957cb147393ac5bd974b9900388f2e3b
Miguel Oduro <migzee321@gmail.com>
2024-12-07 14:44:12 +0000

    qr code project

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -4,27 +4,22 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 
-
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
 
+
 inquirer
   .prompt([
     {
-      message: "Type in your URL: ",
-      name: "URL",
-    },
+    message: "Type in your URL: ",
+    name: "URL",
+  },
   ])
   .then((answers) => {
     const url = answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
-
-    fs.writeFile("URL.txt", url, (err) => {
-      if (err) throw err;
-      console.log("The file has been saved!");
-    });
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -33,5 +28,3 @@ inquirer
       // Something else went wrong
     }
   });
-
-
