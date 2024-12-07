195362dc27cd6ee973a4716f1e358e4000dcdb19
Miguel Oduro <migzee321@gmail.com>
2024-12-07 15:11:51 +0000

    u

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -20,6 +20,10 @@ inquirer
     const url = answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+  fs.writeFile('URL.txt', url, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  }); 
   })
   .catch((error) => {
     if (error.isTtyError) {
